83891077fd20582842c72d538426e0ddae1ecbce
alaaMandour <alaaMandour2024@gmail.com>
2024-07-29 22:27:53 +0300

    complete it

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'layout/game.dart';
+import 'layout/homePage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Game(),
+      home: HomePage(),
     );
   }
 }
